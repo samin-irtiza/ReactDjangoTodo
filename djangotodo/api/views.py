49d3238cc51f20484c8d7e8eb48a0e1f06e2033c
samin-irtiza <62540152+samin-irtiza@users.noreply.github.com>
2023-02-01 23:16:17 +0600

    handled POST request to Add Task

--- djangotodo/api/views.py
+++ djangotodo/api/views.py
@@ -1,6 +1,8 @@
+from urllib import response
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from base.models import *
+from djangotodo import api
 from .serializers import *
 
 
@@ -8,4 +10,11 @@ from .serializers import *
 def getTask(request):
     items=Task.objects.all()
     serializer = TaskSerializer(items, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def addTask(request):
+    serializer=TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
     return Response(serializer.data)

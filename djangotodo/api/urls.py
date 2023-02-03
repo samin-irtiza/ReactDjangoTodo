fcddcc0eb131845b3323a38f3bae852b4a27b4c6
samin-irtiza <62540152+samin-irtiza@users.noreply.github.com>
2023-02-03 23:38:29 +0600

    created CRUD functions for the API

--- djangotodo/api/urls.py
+++ djangotodo/api/urls.py
@@ -2,6 +2,10 @@ from django.urls import URLPattern, path
 from . import views
 
 urlpatterns = [
-    path('',views.getTask),
-    path('add',views.addTask)
+    path('',views.apiOverview),
+    path('task-list/',views.getTaskList),
+    path('task-detail/<str:pk>/',views.getTask),
+    path('task-create/',views.addTask),
+    path('task-update/<str:pk>/', views.updateTask),
+    path('task-delete/<str:pk>/',views.deleteTask),
 ]
--- djangotodo/api/views.py
+++ djangotodo/api/views.py
@@ -6,9 +6,26 @@ from .serializers import *
 
 
 @api_view(['GET'])
-def getTask(request):
-    items=Task.objects.all()
-    serializer = TaskSerializer(items, many=True)
+def apiOverview(request):
+    api_urls = {
+        'List' : 'task-list/',
+        'Detail' : 'task-detail/<str:pk>/',
+        'Create' : 'task-create/',
+        'Update' : 'task-update/<str:pk>/',
+        'Delete' : 'task-delete/<str:pk>/',
+    }
+    return Response(api_urls)
+
+@api_view(['GET'])
+def getTaskList(request):
+    tasks=Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getTask(request,pk):
+    task=Task.objects.get(id=pk)
+    serializer = TaskSerializer(task, many=False)
     return Response(serializer.data)
 
 @api_view(['POST'])
@@ -17,3 +34,20 @@ def addTask(request):
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
+
+@api_view(['POST'])
+def updateTask(request,pk):
+    task=Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task, data=request.data, many=False)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def deleteTask(request,pk):
+    task=Task.objects.get(id=pk)
+    id=task.id
+    task.delete()
+    return Response(f'Task with ID = {id} successfully deleted')

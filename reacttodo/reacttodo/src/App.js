d7690c0b24dc988eb4463f3969fe631a46b7011d
samin-irtiza <62540152+samin-irtiza@users.noreply.github.com>
2023-02-05 23:39:33 +0600

    created form for taking user input

--- reacttodo/reacttodo/src/App.js
+++ reacttodo/reacttodo/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="form-wrapper">
+        {/* <form className='todo-form' onSubmit={handleSubmit}>
+          <input
+            type='text'
+            placeholder='Add a task'
+            value={input}
+            name='text'
+            className='todo-input'
+            onChange={viewChange}
+            ref={focusOnInput}
+          />
+          <button className='todo-button'> Add Task</button>
+        </form> */}
+      </div>
+      <div className="list-wrapper">
+
+      </div>
     </div>
   );
 }

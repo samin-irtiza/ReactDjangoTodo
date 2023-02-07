a788bbb2c19a774022d2eff48fb22156481eac4d
samin-irtiza <62540152+samin-irtiza@users.noreply.github.com>
2023-02-07 23:42:20 +0600

    cross framework connection Check

--- reacttodo/reacttodo/src/App.js
+++ reacttodo/reacttodo/src/App.js
@@ -3,6 +3,18 @@ import React, {useState, useEffect, useRef} from 'react';
 function App() {
 
   const [tasks,setTask] = useState([])
+
+  useEffect(() => {
+    fetchTasks();
+  });
+
+  function fetchTasks(){
+    console.log("Fetching List of Tasks...")
+
+    fetch('http://127.0.0.1:8000/api/task-list/')
+    .then(response => response.json())
+    .then(data => console.log(data));
+  }
   return (
     <div className="App">
       <div className="form-wrapper">
